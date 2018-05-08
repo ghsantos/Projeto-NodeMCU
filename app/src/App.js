21d17125ba8c1356caa03a74c7850be7e832950a
ghsantos <ghsantos933@gmail.com>
2018-05-08 00:16:31 -0300

    update app.js

--- app/src/App.js
+++ app/src/App.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
+import Paper from 'material-ui/Paper';
 
-const url = process.env.NODE_ENV === 'production' ? "/api/" : "http://10.130.1.9:5000/api/";
+const url = process.env.NODE_ENV === 'production' ? "/api/" : "http://localhost:5000/api/";
 
 const style = {
   margin: 12,
   width: 180,
+};
+
+const paperStyle = {
+  height: 240,
+  width: 450,
+  margin: 20,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
 };
 
 class App extends Component {
@@ -15,16 +26,16 @@ class App extends Component {
     super(props);
 
     this.state = {
-      luminosidade: '',
-      temperatura: '',
-      umidade: '',
+      luminosidade: '68',
+      temperatura: '25.3',
+      umidade: '78',
       led1on: 0,
       led2on: 0,
     }
   }
 
   componentDidMount() {
-    this.getClima();
+    //this.getClima();
     this.getStatus();
   }
 
@@ -75,24 +86,25 @@ class App extends Component {
     return (
       <div>
         <AppBar
-          title="Title"
+          title="NodeMCU App"
           showMenuIconButton={false}
         />
 
         <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-          <div>
-            <div>
-              <span>Temperatura: {this.state.temperatura} °C</span>
-            </div>
 
-            <div>
-              <span>Umidade: {this.state.umidade}%</span>
-            </div>
+          <Paper zDepth={2} style={paperStyle}>
+              <div>
+                <span style={{fontSize: 90}}>{this.state.temperatura} °C</span>
+              </div>
 
-            <div>
-              <span>Luminosidade: {this.state.luminosidade}%</span>
-            </div>
-          </div>
+              <div>
+                <span style={{fontSize: 23}}>Umidade: {this.state.umidade}%</span>
+              </div>
+
+              <div>
+                <span style={{fontSize: 23}}>Luminosidade: {this.state.luminosidade}%</span>
+              </div>
+          </Paper>
 
           <div>
             <RaisedButton
@@ -115,8 +127,10 @@ class App extends Component {
               primary={true}
               style={style}
               onClick={() => {
-                this.setState({ led1on: this.state.led1on === 1 ? 0 : 1 });
-                this.postStatus();
+                this.setState(
+                  { led1on: this.state.led1on === 1 ? 0 : 1 },
+                  () => this.postStatus()
+                );
               }}
             />
 
@@ -125,8 +139,10 @@ class App extends Component {
               primary={true}
               style={style}
               onClick={() => {
-                this.setState({ led2on: this.state.led2on === 1 ? 0 : 1 });
-                this.postStatus();
+                this.setState(
+                  { led2on: this.state.led2on === 1 ? 0 : 1 },
+                  () => this.postStatus()
+                );
               }}
             />
           </div>
